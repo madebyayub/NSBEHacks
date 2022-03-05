b0b94599da31cf9467a244a5b4e50fde50f4f1ba
Jeremy Reist <jeremyreist@gmail.com>
2022-03-05 14:05:12 -0500

    updated starter template

--- leetcode_solver/main/urls.py
+++ leetcode_solver/main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from main.views import index
+from main.views import index, problem
 
 urlpatterns = [
     path('', index),
+    path('<int:id>/', problem)
 ]
--- leetcode_solver/main/views.py
+++ leetcode_solver/main/views.py
@@ -5,7 +5,23 @@ from django.template.response import TemplateResponse
 def index(request):
     # User requests the home page
     if request.method == 'GET':
-        return TemplateResponse(request, 'main/index.html', context={"example": "-- some data --"})
+        return TemplateResponse(request, 'main/index.html', context={"example": "-- some data --", "example2": "-- some other data --"})
 
     elif request.method == 'POST':
+        
+        pass
+
+# Create your views here.
+def problem(request, id):
+
+    # Handle getting video data
+
+
+
+    # User requests the home page
+    if request.method == 'GET':
+        return TemplateResponse(request, 'main/index.html', context={"youtube_link": id, "title": id})
+
+    elif request.method == 'POST':
+        
         pass
